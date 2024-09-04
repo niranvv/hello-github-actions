055ee9b4f8c8b0214923b6891f9e7e008378fab7
Niran Vijayakumar <niranvv@gmail.com>
2024-09-04 19:29:28 +0530

    Code cleanup & Added reusable CD

--- src/web/server.js
+++ src/web/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000; // Unnecessary now, but useful once we start running our server in the cloud later on, per https://learn.microsoft.com/en-us/azure/app-service/configure-language-nodejs?pivots=platform-linux#get-port-number
+const port = process.env.PORT || 3000;
 
 app.get(["/", "/index.html"], (req, res) => {
     res.send("Hello World!");
@@ -12,4 +12,4 @@ if (process.env.NODE_ENV !== "test") {
     });
 }
 
-module.exports = app; // Unnecessary now, but useful once we start adding unit tests later on, per https://stackoverflow.com/a/33988296
+module.exports = app;
